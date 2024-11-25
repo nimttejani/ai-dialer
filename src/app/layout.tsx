68150ca8e7c11f378173c15e76d8f25f90127c49
John George <askjohngeorge@gmail.com>
2024-11-25 08:35:13 +0000

    Delete <head />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head />
       <body className={inter.className}>
         <Providers>
           <AuthAwareLayout>{children}</AuthAwareLayout>
